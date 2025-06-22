bac6105207eebb2b4ceddfa774a2b2bb3c38be60
Thomas Eby <tgeby@icloud.com>
2025-06-21 22:04:38 -0400

    fix

--- study-app/src/firebase.js
+++ study-app/src/firebase.js
@@ -9,7 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC0c4sxT9GOyOV3KIzBjr7iS38LBNg2DDs",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "study-tool-b4548.firebaseapp.com",
   projectId: "study-tool-b4548",
   storageBucket: "study-tool-b4548.firebasestorage.app",
