a258ebf9f62c85723e8ae3e0d6b5144ae874e7aa
Thomas Eby <tgeby@icloud.com>
2025-06-28 16:13:30 -0400

    Added functionality to publish updates to a deck

--- study-app/src/components/EditDeck.jsx
+++ study-app/src/components/EditDeck.jsx
@@ -5,6 +5,8 @@ import { useAuth } from '../context/AuthContext';
 import { useState, useEffect } from 'react';
 import { useDeck } from '../hooks/useDeck';
 import DeckEditor from './DeckEditor';
+import { db } from '../firebase';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 function EditDeck() {
 
@@ -15,10 +17,29 @@ function EditDeck() {
     const { deckData, loading, error } = useDeck(deckId, user);
     const [currentName, setCurrentName] = useState('');
     const [currentDeck, setCurrentDeck] = useState([]);
+    const [statusMessage, setStatusMessage] = useState('');
 
     async function handlePublishDeck() {
         console.log('update deck');
-        navigate('/menu');
+        if (!deckId) {
+            console.log('tried to upload a deck with no deckID');
+        }
+        try {
+            await setDoc(doc(db, 'flashcard_sets', deckId), {
+                name: currentName.trim(),
+                cards: currentDeck,
+                updated_at: serverTimestamp()
+            }, {merge: true});
+
+            setStatusMessage('Successfully updated deck');
+
+            setTimeout(() => {
+                navigate('/menu');
+            }, 1500);
+        } catch (error) {
+            setStatusMessage('Update deck failed');
+            console.log(error);
+        } 
     }
 
     useEffect(() => {
@@ -37,6 +58,7 @@ function EditDeck() {
     return (
         <div className='main'>
             <div className='title'>Edit Deck</div>
+            {statusMessage && <p className="status-message">{statusMessage}</p>}
 
             {currentDeck.length > 0 && 
                 <DeckEditor
