4c5819d3d96507508773bd60594d30e8aa7b3b0a
Thomas Eby <tgeby@icloud.com>
2025-06-30 16:46:11 -0400

    Added coming soon labels to other study modes

--- study-app/src/components/StudyMenu.jsx
+++ study-app/src/components/StudyMenu.jsx
@@ -1,4 +1,5 @@
 import '../styles/global-styles.css';
+import './StudyMenu.css';
 
 import { useNavigate } from 'react-router-dom';
 
@@ -17,8 +18,14 @@ function StudyMenu() {
                 Study Mode
             </div>
             <button type="button" className="menu-button" onClick={() => handleModeSelect('flashcards')}>Flashcards</button>
-            <button type="button" className="menu-button" onClick={() => handleModeSelect('memory-game')}>Memory Game</button>
-            <button type="button" className="menu-button" onClick={() => handleModeSelect('test')}>Test</button>
+            <button type="button" className="menu-button disabled" onClick={() => handleModeSelect('memory-game')} disabled='true'>
+                Memory Game
+                <p style={{ fontSize: '1rem', opacity: 0.7 }}>Coming soon...</p>
+            </button>
+            <button type="button" className="menu-button disabled" onClick={() => handleModeSelect('test')} disabled='true'>
+                Test
+                <p style={{ fontSize: '1rem', opacity: 0.7 }}>Coming soon...</p>
+            </button>
         </div>
     );
 }
